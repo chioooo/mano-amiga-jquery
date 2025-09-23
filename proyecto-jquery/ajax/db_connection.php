65cb46089f6e729c382682664227b39bba3c801c
chioooo <mduran43@gmail.com>
2025-09-22 20:29:25 -0600

    quitando puerto

--- proyecto-jquery/ajax/db_connection.php
+++ proyecto-jquery/ajax/db_connection.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
-$port = 3307;
 $username = "mano_admin";
 $password = "MiClave123*";
 $dbname = "mano_amiga";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     header('Content-Type: application/json; charset=utf-8');
